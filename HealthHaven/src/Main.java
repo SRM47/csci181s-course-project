f3efd8217cbef43687762233476a6994374636d6
Sam <sameerrajmalik@gmail.com>
2024-02-29 16:10:22 -0800

    Tabular fib implementation

--- HealthHaven/src/Main.java
+++ HealthHaven/src/Main.java
@@ -1,8 +1,24 @@
 
 public class Main {
 
+	public static int fib(int n) {
+		if (n <= 0) {
+			return -1;
+		}
+		int[] fib = new int[n + 1];
+		fib[0] = 1;
+		fib[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+		return fib[n];
+	}
+
 	public static void main(String[] args) {
-		System.out.println("This is secure!");
+		System.out.println("This is secure!\n");
+		
+		int num = fib(47);
+		System.out.print("This is the 47th fibbonacci number: " + num);
 	}
 
 }
