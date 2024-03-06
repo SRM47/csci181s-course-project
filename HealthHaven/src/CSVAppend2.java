77763474539bea326522b14618e1fa607fd52cef
max-baum <miba2020@mymail.pomona.edu>
2024-03-06 12:03:46 -0800

    CSVappendadd

---
+++ HealthHaven/src/CSVAppend2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CSVAppend2{ 
+
+    public void appendToCSV1(String newData) {
+        String csvFilePath = "db.csv";
+        
+        try {
+            appendToCSV2(csvFilePath, newData);
+            System.out.println("Data appended to CSV successfully.");
+        } catch (IOException e) {
+            System.err.println("Error appending data to CSV: " + e.getMessage());
+        }
+    }
+
+    private static void appendToCSV2(String filePath, String newData) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            // Append the new data to the end of the CSV file
+            writer.write(newData); 
+            writer.newLine();
+        }
+    }
+}
+
