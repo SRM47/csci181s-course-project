f9b5b5711048bace2b5434dfdabb00fed1a0e15b
Sam <sameerrajmalik@gmail.com>
2024-03-24 11:49:07 -0700

    Moved database config file to appropriate package

--- HealthHaven/src/database/DatabaseConfig.java
+++ HealthHaven/src/database/DatabaseConfig.java
@@ -8,7 +8,7 @@ public class DatabaseConfig {
 	private static final Properties properties = new Properties();
 
 	static {
-		try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("./db.properties")) {
+		try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("./database/db.properties")) {
 			if (input == null) {
 				System.out.println("Sorry, unable to find db.properties");
 				System.exit(1);
