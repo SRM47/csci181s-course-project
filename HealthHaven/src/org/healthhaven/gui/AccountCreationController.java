6448d0eee83689b299309909cf2c9ead0f1f2269
max-baum <miba2020@mymail.pomona.edu>
2024-03-25 12:55:35 -0700

    starting gui

---
+++ HealthHaven/src/org/healthhaven/gui/AccountCreationController.java
@@ -0,0 +1,24 @@
+package org.healthhaven.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AccountCreationController {
+	
+	@FXML
+	private TextField textfield;
+	@FXML
+	private Label label;
+	@FXML
+	private Button submitbutton;
+	
+	
+	public void handleSubmit() {
+		label.setText(textfield.getText());
+	}
+	
+	
+
+}
