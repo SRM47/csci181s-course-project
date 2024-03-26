e66d884102b707c2e5ee037719cf9f62a35e18c5
Sae Furukawa <sfaa2021@mymail.pomona.edu>
2024-03-26 11:55:33 -0700

    Finished all fxml files, moving into debugging stage.

--- HealthHaven/src/org/healthhaven/controller/UserProfileController.java
+++ HealthHaven/src/org/healthhaven/controller/UserProfileController.java
@@ -5,11 +5,12 @@ import org.healthhaven.model.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class UserProfileController {
 	@FXML
-    private Label userInfoLabel;
+    private TextArea userInfoArea;
     @FXML
     private TextField emailField;
     @FXML
@@ -21,12 +22,13 @@ public class UserProfileController {
 
     public void setUser(User user) {
         this.currentUser = user;
+        displayUserInfo();
         // Update the view with user information
     }
     
     private void displayUserInfo() {
         // Assuming User has a toString() or a similar method to get a formatted string of user info
-        userInfoLabel.setText(currentUser.toString());
+    	userInfoArea.setText(currentUser.toString());
     }
 
     // Methods to handle profile updates
@@ -36,6 +38,14 @@ public class UserProfileController {
         String email = emailField.getText();
         String password = passwordField.getText();// get from input field;
         String address = addressField.getText();// get from input field;
+        
+        if (email.equals("")) {
+        	email = currentUser.getEmail();
+        } if (password.equals("")) {
+        	password = currentUser.getPassword();
+        } if (address.equals("")) {
+        	address = currentUser.getAddress();
+        }
 
         // Update User model
         currentUser.updatePersonalRecordOnDB(email, password, address);
