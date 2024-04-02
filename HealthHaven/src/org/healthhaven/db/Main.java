cb3609708a429f8c94866cbcfed4baf0e9d5ad35
Ethan Lee <ethethanlee@gmail.com>
2024-04-02 01:29:13 -0700

    made changes around password being removed and looked at db stuff to get a better idea how to DP

--- HealthHaven/src/org/healthhaven/db/Main.java
+++ HealthHaven/src/org/healthhaven/db/Main.java
@@ -20,7 +20,7 @@ public class Main {
 //            boolean succeeded = UserDAO.createUser(connection, p);
 //            System.out.println(succeeded);
             User u = UserDAO.getUserInformation(connection, "1234567890");
-            System.out.println("Connected to the PasdfostgreSQL database.");
+//            System.out.println("Connected to the PasdfostgreSQL database.");
             System.out.println(u);
         } catch (SQLException e) {
             System.err.println(e.getMessage());
