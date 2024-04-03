0129b224a5ad16eb2040dc9a2219b74fa3516f8a
max-baum <miba2020@mymail.pomona.edu>
2024-04-02 17:52:40 -0700

    totp updates

---
+++ HealthHaven/src/org/healthhaven/model/TOTP.java
@@ -0,0 +1,54 @@
+package org.healthhaven.model;
+
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.time.Instant;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+
+import com.eatthepath.otp.TimeBasedOneTimePasswordGenerator;
+
+public class TOTP {
+	
+	final static TimeBasedOneTimePasswordGenerator totp = new TimeBasedOneTimePasswordGenerator();
+	final static Instant now = Instant.now();
+	final static Duration timestep = totp.getTimeStep();
+	
+	public static Key secretKey() throws NoSuchAlgorithmException {
+		
+		KeyGenerator keyGenerator = KeyGenerator.getInstance(totp.getAlgorithm());
+		
+		final int macLengthInBytes = Mac.getInstance(totp.getAlgorithm()).getMacLength();
+		keyGenerator.init(macLengthInBytes * 8);
+		
+		Key key = keyGenerator.generateKey();
+		
+		return key;
+	}
+	
+	
+	public static String genTOTP(Key key) throws InvalidKeyException {
+		
+		String pass = totp.generateOneTimePasswordString(key, now);
+		return pass;	
+	}
+	
+	
+	public static int verTOTP(Key key, String pass) throws InvalidKeyException {
+		
+		for(int i=0; i<6; i++) {
+			Instant checktime = now.minus(timestep.multipliedBy(i));
+			
+			String totpCheck = totp.generateOneTimePasswordString(key, checktime);
+			
+			if (pass.equals(totpCheck)){
+				return 1;
+			}
+		}
+		return 0;
+		
+	}
+}
