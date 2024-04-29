ea22991b848b7b079730fc2ad2fdb6a41d115ee0
Ethan Lee <ethethanlee@gmail.com>
2024-04-29 16:23:48 -0700

    push but it doesnt work

--- HealthHaven/src/org/healthhaven/model/MedicalDataInstance.java
+++ HealthHaven/src/org/healthhaven/model/MedicalDataInstance.java
@@ -8,6 +8,7 @@ import java.util.Date;
 
 import javafx.beans.property.SimpleFloatProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 
 /**
  * 
@@ -16,17 +17,19 @@ public class MedicalDataInstance {
 	
 	@Override
 	public String toString() {
-		return "MedicalDataInstance [height=" + height + ", weight=" + weight + ", timestamp=" + timestamp + "]";
+		return "MedicalDataInstance [height=" + height + ", weight=" + weight + ", timestamp=" + timestamp + ", identifier=" + identifier + "]";
 	}
 
 	private SimpleFloatProperty height;
 	private SimpleFloatProperty weight;
 	private SimpleObjectProperty<Date> timestamp;
+	private SimpleStringProperty identifier;
 	
-	public MedicalDataInstance(float height, float weight, Date timestamp) {
+	public MedicalDataInstance(float height, float weight, Date timestamp, String identifier) {
 		this.height = new SimpleFloatProperty(height);
 		this.weight = new SimpleFloatProperty(weight);
 		this.timestamp = new SimpleObjectProperty<Date>(timestamp);
+		this.identifier = new SimpleStringProperty(identifier);
 	}
 	
 	// Getters 
@@ -52,6 +55,14 @@ public class MedicalDataInstance {
     
     public SimpleObjectProperty<Date> timestampProperty() {
         return timestamp;
+    }
+    
+    public String getIdentifier() {
+        return identifier.get();
+    }
+
+    public SimpleStringProperty identifierProperty() {
+        return identifier;
     }
 
 
