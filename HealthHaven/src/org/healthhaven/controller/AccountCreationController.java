fa09016c84ed21bebcadb2d0c69fe57e2879e692
Ethan Lee <ethethanlee@gmail.com>
2024-04-30 00:02:20 -0700

    bee once again

--- HealthHaven/src/org/healthhaven/controller/AccountCreationController.java
+++ HealthHaven/src/org/healthhaven/controller/AccountCreationController.java
@@ -29,7 +29,7 @@ import javafx.stage.Stage;
 import me.gosimple.nbvcxz.Nbvcxz;
 import me.gosimple.nbvcxz.scoring.Result;
 
-// asdfasdf penis
+// asdfasdf bee movie 
 
 public class AccountCreationController {
 	@FXML
