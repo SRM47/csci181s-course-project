7f2022b32bc2db9f5288693f220c3f99b7e87269
Sam <sameerrajmalik@gmail.com>
2024-04-29 23:07:08 -0700

    Improved reference monitor and authorization

--- HealthHaven/src/org/healthhaven/server/ReferenceMonitor.java
+++ HealthHaven/src/org/healthhaven/server/ReferenceMonitor.java
@@ -20,10 +20,12 @@ public class ReferenceMonitor {
 	
 	protected static boolean authorizeRequest(SSLSocket clientSocket, String accountType, String callerId, JSONObject json, Connection conn) {
 		boolean res = false;
+		String reason = "Unauthorized access";
 		switch (json.getString("request")) {
 		case "UPDATE_DATA_SHARING":
 			if (accountType.equals("Patient")) {
 				res = true;
+				reason = "";
 			} else {
 				res = false;
 			}
@@ -31,8 +33,10 @@ public class ReferenceMonitor {
 		case "ALLOW_ACCOUNT_CREATION": 
 			if (accountType.equals("Superadmin")) {
 				res = true;
+				reason = "";
 			} else if (accountType.equals("Doctor")) {
-				res = json.getString("userType").equals("Patient");
+				res = "Patient".equals(json.optString("userType"));
+				reason = res ? "" : "User type trying to be accessed is not a patient";
 			} else {
 				res = false;
 			}
@@ -40,47 +44,59 @@ public class ReferenceMonitor {
 			
 		case "REQUEST_PATIENT_DATA": //data analyst	
 			res = accountType.equals("Data Analyst");
+			reason = res ? "" : "Account type is not Data Analyst";
 			break;
 			
 		case "VIEW_RECORD": //doctor or patient
 			if (accountType.equals("Patient")) {
-				res = json.getString("patientID").equals(callerId);
+				res = callerId.equals(json.optString("patientID"));
+				reason = res ? "" : "Caller is not the patient whoes data is being accessed";
 			} else if (accountType.equals("Doctor")) {
-				res = AccountDAO.isDoctorAuthorizedToViewPatientData(conn, json.getString("doctorID"), json.getString("patientID")).getString("result").equals("SUCCESS");
+				res = AccountDAO.isDoctorAuthorizedToViewPatientData(conn, json.optString("doctorID"), json.optString("patientID")).getString("result").equals("SUCCESS");
+				reason = res ? "" : "Doctor is unauthorized to view this patient's data";
 			} else {
 				res = false;		
 			}
 			break;
 			
 		case "CREATE_RECORD": //doctor
-			res = accountType.equals("Doctor") && AccountDAO.isDoctorAuthorizedToViewPatientData(conn, json.getString("doctorID"), json.getString("patientID")).getString("result").equals("SUCCESS");
+			res = accountType.equals("Doctor") && AccountDAO.isDoctorAuthorizedToViewPatientData(conn, json.optString("doctorID"), json.optString("patientID")).getString("result").equals("SUCCESS");
+			reason = res ? "" : "Doctor is unauthorized to create data for this patient";
 			break;
 			
 		case "DEACTIVATE_ACCOUNT": //any user for themselves or admin for everyone
-			if (json.getString("type").equals("VALIDATE_ACCOUNT")) {
-				res = true;
-			}
 			if (accountType.equals("Superadmin")) {
 				res = true;
+				reason = "";
 			} else {
-				res = json.getString("callerId").equals(callerId);
+				if ("VALIDATE_ACCOUNT".equals(json.optString("type"))) {
+					// Check if the userID associated with the email is equal to the caller's ID
+					JSONObject userIdFromEmail = AccountDAO.verifyUserIdfromEmail(conn, callerId, json.optString("email"));
+					res = userIdFromEmail.getString("result").equals("SUCCESS");
+					reason = userIdFromEmail.getString("reason");
+				} else if ("DEACTIVATE_ACCOUNT".equals(json.optString("type"))) {
+					res = callerId.equals(json.optString("userId"));
+					reason = res ? "" : "Caller Id does not match the user id being deactivated";
+				}
 			}
 			break;
 			
 		case "SEARCH_ACCOUNT": //super admin
 			res = accountType.equals("Superadmin");
+			reason = res ? "" : "Must be a superadmin";
 			break;
 			
 		case "LOGIN","PASSWORD_RESET","CREATE_ACCOUNT","UPDATE_ACCOUNT","LOGOUT":
 			res = true;
+			reason = "";
 			break;
 		default:
 			res = false;	
 			break;
 		}
 		
-		String logMessage = String.format("request:%s caller_id:%s result:%s reason:%s\n", json.getString("request"),
-				callerId, res ? "SUCCESS" : "FAILURE", "");
+		String logMessage = String.format("request:%s caller_id:%s result:%s reason:%s\n", json.optString("request"),
+				callerId, res ? "SUCCESS" : "FAILURE", reason);
 		log(clientSocket, logMessage);
 		return res;
 	}
