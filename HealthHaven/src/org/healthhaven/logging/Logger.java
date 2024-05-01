fea3cc40c3ed6bed7d2138994adabf536d021409
Sam <sameerrajmalik@gmail.com>
2024-04-30 20:52:30 -0700

    Merge branch 'main' of https://github.com/SRM47/csci181s-course-project.git into main

--- HealthHaven/src/org/healthhaven/logging/Logger.java
+++ HealthHaven/src/org/healthhaven/logging/Logger.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class Logger {
 	
 
-    public static void log(String filepath, String clientIP, String message) {
+    public static synchronized void log(String filepath, String clientIP, String message) {
         try (FileWriter fileWriter = new FileWriter(filepath, true)) {
             String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
             String logEntry = String.format("[%s] - %s - %s\n", timestamp, clientIP, message);
